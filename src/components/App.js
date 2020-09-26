fba650e0bbfb2a207b168a12d1e3e8d06ed0828f
Shubham Serva <M1047224@mindtree.com>
2020-09-26 09:53:20 +0530

    initial code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component{
 
+    state = {
+        language: 'english'
+    }
+
+    onLanguageChange = language =>{
+        console.log("state value",this.state.language);
+        this.setState({ language: language});
+        
+    }
+
     render(){
         return(
-            <div></div>
+            <div className="ui container">
+                <div>
+                Select a language
+                <i className="flag us" onClick={()=>this.onLanguageChange('english')}/>
+                <i className="flag nl" onClick={()=>this.onLanguageChange('dutch')}/>
+             </div>
+             <LanguageContext.Provider value={this.state.language}>
+                 <ColorContext.Provider value= 'red'>
+                <UserCreate/>
+                </ColorContext.Provider>
+             </LanguageContext.Provider>
+           
+            </div>
         )
     }
 }
